6861454086fa9f25ecf892ec82e05888742ae95a
Krishnakant Sonakiya <30476198+kksonakiya@users.noreply.github.com>
2023-08-20 12:06:04 +0530

    changes

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
 from apps import create_app
 
-from flaskwebgui import FlaskUI
+# from flaskwebgui import FlaskUI
 
-app=create_app()
+app = create_app()
 
-if __name__ == '__main__':
-    # app.run(debug=True, port=5020, host='0.0.0.0')
-    FlaskUI(app=app, server="flask").run()    
+if __name__ == "__main__":
+    app.run(debug=True, port=5020, host="0.0.0.0")
+    # FlaskUI(app=app, server="flask").run()
